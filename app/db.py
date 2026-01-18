74ffeb74d746bbeb054ee220dea6d25c5fd888ed
xhdndmm <xhdndmm@gmail.com>
2026-01-18 18:55:15 +0800

    修复部分问题

--- app/db.py
+++ app/db.py
@@ -9,7 +9,7 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True, nullable=True, index=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(256), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
